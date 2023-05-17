c1d6d7ad0c74e370b20b834fe9ca364c13843897
Oreste Luci <github@luci.cl>
2023-05-17 19:29:53 +0200

    Roll Dice App

---
+++ lib/gradient_container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.startColor, this.endColor, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : startColor = const Color.fromARGB(255, 26, 2, 80),
+        endColor = const Color.fromARGB(255, 45, 7, 98);
+
+  final Color startColor;
+  final Color endColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [startColor, endColor],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
